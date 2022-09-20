ca6b92753bbf922d1c537f696afbf1273c559d7e
tihomirovsk <pesokote@gmail.com>
2022-09-20 20:41:23 +0300

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число.");
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k < 0 ? "третьей цифры нет" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine();
